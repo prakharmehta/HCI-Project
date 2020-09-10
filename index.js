4a80331e0bfa6f0e556790b687f43654ff87cff3
prakharmehta <prkh.mehta@gmail.com>
2020-09-10 18:07:21 +0530

    Added text summarization

--- index.js
+++ index.js
@@ -3,30 +3,33 @@ const {spawn} = require('child_process');
 const app = express()
 const port = 3000
 
-app.set('views','./views');
-app.set('view-engine', 'ejs');
+// app.set('views','./views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
  
- var dataToSend;
- // spawn new child process to call the python script
- const python = spawn('python', ['scripts/script1.py']);
- // collect data from script
- python.stdout.on('data', function (data) {
-  console.log('Pipe data from python script ...');
-  dataToSend = data.toString();
+  res.send("Hey")
  });
- // in close event we are sure that stream from child process is closed
- python.on('close', (code) => {
- console.log(`child process close all stdio with code ${code}`);
- // send data to browser
- res.send(dataToSend)
- });
- 
-})
 
 app.get('/tts', (req, res) => {
-    res.render('textToSpeech.ejs')
+    let dataToSend;
+    // spawn new child process to call the python script
+    const python = spawn('python', ['scripts/text_summarization.py']);
+    // collect data from script
+    python.stdout.on('data', function (data) {
+     console.log('Pipe data from python script ...');
+     dataToSend = data.toString();
+     console.log(dataToSend);
+     res.render('textToSpeech', {
+        textToBeSummarized: dataToSend
+    })
+    });
+    python.stderr.on('data',(data) => console.log(data.toString()))
+    // in close event we are sure that stream from child process is closed
+    python.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    })
+    
 })
-app.listen(port, () => console.log(`Example app listening on port 
-${port}!`))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
