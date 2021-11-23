0b223ae26bbb09427c01712332829d6579c99ba1
Prakhar Mehta <prakh.mehta@gmail.com>
2021-11-23 12:44:23 +0530

    switching

--- public/scripts/textTranslation.py
+++ public/scripts/textTranslation.py
@@ -1,12 +1,10 @@
-from google_trans_new import google_translator  
+from googletrans import Translator
 import sys
 
-# translator = Translator()
-  
-translator = google_translator()
+translator = Translator()
 # outputFile = open(sys.argv[1])
 data = sys.argv[1]
-translatedOutput = translator.translate(data, lang_src='en', lang_tgt=sys.argv[2])
+translatedOutput = translator.translate(data, dest=sys.argv[2])
     # with open("./models/translationOutput/translatedText.txt", "w") as t:
     #     t.write(str(translatedOutput.text))
 
